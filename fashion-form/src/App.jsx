558f922e98621da6af850c4031fc360eb720ed8f
RYacoub25 <reemmanassa.rm202@gmail.com>
2025-12-03 16:15:10 +0200

    still fixing the date part

--- fashion-form/src/App.jsx
+++ fashion-form/src/App.jsx
@@ -291,7 +291,7 @@ function Input(props) {
   return (
     <input
       {...props}
-      className={`w-full rounded-xl bg-black/5 border border-black/15 focus:border-black/40 focus:outline-none px-4 py-3 placeholder-black/30 shadow-inner ${props.className || ""
+      className={`w-full max-w-full rounded-xl bg-black/5 border border-black/15 focus:border-black/40 focus:outline-none px-4 py-3 placeholder-black/30 shadow-inner ${props.className || ""
         }`}
     />
   );
